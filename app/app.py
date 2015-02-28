b24c037cab5a07548b009beca26f2dc466753077
Xilin Liu <xal1@rice.edu>
2015-02-28 12:40:04 -0600

    make final pull request

--- app/app.py
+++ app/app.py
@@ -21,32 +21,31 @@ cur = con.cursor()
 def index():
     return app.send_static_file("index.html")
 
-@app.route("/getContacts")
-def get_contacts():
-    with con:
-        cur.execute("""SELECT * FROM contact""")
-        rows = {"result": cur.fetchall()}
-        return jsonify(rows)
-    return jsonify({})
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "GET":
+        with con:
+            cur.execute("""SELECT * FROM contact""")
+            rows = {"result": cur.fetchall()}
+            return jsonify(rows)
+        return jsonify({})
+    elif request.method == "POST":
+        f = request.form
+        with con:
+            cur.execute("""INSERT INTO contact (firstName, lastName, phone, address, city, state, zip)
+                           VALUES (?, ?, ?, ?, ?, ?, ?)""",
+                           (f['first'], f['last'], f['phone'], f['address'], f['city'], f['state'], f['zip']))
+            con.commit()
+            cur.execute("""SELECT * FROM contact WHERE contactId = ?""", (str(cur.lastrowid),))
+            contact = {"result": [cur.fetchone()]}
+            return jsonify(contact)
+        return jsonify({})
 
-@app.route("/addContact", methods = ["POST"])
-def add_contact():
-    f = request.form
-    with con:
-        cur.execute("""INSERT INTO contact (firstName, lastName, phone, address, city, state, zip)
-                       VALUES (?, ?, ?, ?, ?, ?, ?)""",
-                       (f['first'], f['last'], f['phone'], f['address'], f['city'], f['state'], f['zip']))
-        con.commit()
-        cur.execute("""SELECT * FROM contact WHERE contactId = ?""", (str(cur.lastrowid),))
-        contact = {"result": [cur.fetchone()]}
-        return jsonify(contact)
-    return jsonify({})
-
-@app.route("/deleteContact/<int:contact_id>")
+@app.route("/contact/<int:contact_id>", methods=["DELETE"])
 def delete_contact(contact_id):
     with con:
         cur.execute("""DELETE FROM contact WHERE contactId = ?""", (str(contact_id),))
     return "deleted " + str(contact_id)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
